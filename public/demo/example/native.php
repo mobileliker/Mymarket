c9e606bd1a118c4e02ad12ecb3b0c8f2966c842d
limingcun <1521010816@qq.com>
2017-02-24 18:45:08 +0800

    [MOD]修改Wxpay.jsApiPay.php

--- public/demo/example/native.php
+++ public/demo/example/native.php
@@ -28,6 +28,7 @@ $url1 = $notify->GetPrePayUrl("123456789");
  * 4、在支付成功通知中需要查单确认是否真正支付成功（见：notify.php）
  */
 $input = new WxPayUnifiedOrder();
+$notify=new NativePay();
 $input->SetBody("test");
 $input->SetAttach("test");
 $input->SetOut_trade_no(WxPayConfig::MCHID.date("YmdHis"));
@@ -35,7 +36,7 @@ $input->SetTotal_fee("1");
 $input->SetTime_start(date("YmdHis"));
 $input->SetTime_expire(date("YmdHis", time() + 600));
 $input->SetGoods_tag("test");
-$input->SetNotify_url("http://paysdk.weixin.qq.com/example/notify.php");
+$input->SetNotify_url("http://www.caishi360.com/demo/example/native_notify.php");
 $input->SetTrade_type("NATIVE");
 $input->SetProduct_id("123456789");
 $result = $notify->GetPayUrl($input);
