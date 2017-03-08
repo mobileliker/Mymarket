b50e9bab60ae5c7d4e9e7f857b71a38d7f14be9c
limingcun <1521010816@qq.com>
2017-03-08 17:54:16 +0800

    [MOD]

--- public/WPay/demo/notify_url.php
+++ public/WPay/demo/notify_url.php
@@ -15,17 +15,20 @@
 	//存储微信的回调
 	$xml = $GLOBALS['HTTP_RAW_POST_DATA'];	
 	$notify->saveData($xml);
-	echo 22;
+	
 	//验证签名，并回应微信。
 	//对后台通知交互时，如果微信收到商户的应答不是成功或超时，微信认为通知失败，
 	//微信会通过一定的策略（如30分钟共8次）定期重新发起通知，
 	//尽可能提高通知的成功率，但微信不保证通知最终能成功。
 	if($notify->checkSign() == FALSE){
+                echo 00;
 		$notify->setReturnParameter("return_code","FAIL");//返回状态码
 		$notify->setReturnParameter("return_msg","签名失败");//返回信息
 	}else{
+                echo 11;
 		$notify->setReturnParameter("return_code","SUCCESS");//设置返回码
 	}
+        echo 22;
 	$returnXml = $notify->returnXml();
 	echo $returnXml;
 	
