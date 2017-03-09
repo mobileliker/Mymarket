440ec4e1a53c228f3ccbc7d663aba824c82c21e9
limingcun <1521010816@qq.com>
2017-03-09 13:53:19 +0800

    jhfghf

--- resources/views/szy/pay/weixin.blade.php
+++ resources/views/szy/pay/weixin.blade.php
@@ -107,6 +107,7 @@
                 },
                 dataType: "json",//json等等
                 success: function (data) {
+                    console.log(data);
                     if(data.status=='paid') {
                         window.location.href="http://www.caishi360.com/user/orders/pay/successful">;
                     }
